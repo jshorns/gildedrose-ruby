06059e235fd8cd67b05b4c5bf4068b85f7abfac3
Joey <joey.s.hornsby@gmail.com>
2021-04-29 09:55:20 +0100

    added test for sell_in decreasing by 1

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,10 +3,16 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+    describe 'for normal items' do
+      items = [Item.new("foo", 3, 4)]
+      shop = GildedRose.new(items)
+      it "does not change the name" do
+        shop.update_quality
+        expect(items[0].name).to eq "foo"
+      end
+      it "diminishes sellIn by 1" do
+        expect {shop.update_quality }.to change {items[0].sell_in }.by -1
+      end
     end
   end
 
