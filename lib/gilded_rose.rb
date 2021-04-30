0386e43ddf343777d92f818fa2c5c964f3fcaee1
Joey <joey.s.hornsby@gmail.com>
2021-04-30 10:38:23 +0100

    refactored increasing and decreasing quality into private methods to keep code DRY

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,24 +7,18 @@ class GildedRose
   def update_quality()
     @items.map do |item|
       next if item.name == "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
+      item.sell_in -= 1
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
+        decrease_quality(item)
       else
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              increase_quality(item)
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              increase_quality(item)
             end
           end
         end
@@ -32,20 +26,28 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-                item.quality = item.quality - 1
-            end
+            decrease_quality(item)
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          increase_quality(item)
         end
       end
     end
   end
+
+  private
+
+  def increase_quality(item)
+    item.quality += 1 unless item.quality >= 50
+  end
+
+  def decrease_quality(item)
+    item.quality -= 1 unless item.quality <= 0
+  end
+
+
 end
 
 class Item
