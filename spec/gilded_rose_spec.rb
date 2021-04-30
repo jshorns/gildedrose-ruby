98cc9e5f4a9bf917c50369ed85032ae8f511fb67
Joey <joey.s.hornsby@gmail.com>
2021-04-30 10:14:02 +0100

    deleted  duplicate tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,7 +7,11 @@ describe GildedRose do
     describe 'for normal items' do
 
       before(:each) do
-        @items = [Item.new("foo", 3, 4), Item.new("bar", 0, 2), Item.new("baz", 0, 0)]
+        @items = [
+          Item.new("foo", 3, 4), 
+          Item.new("bar", 0, 2), 
+          Item.new("baz", 0, 0)
+          ]
         @shop = GildedRose.new(@items)
       end
 
@@ -33,7 +37,12 @@ describe GildedRose do
     describe 'for aged brie' do
 
       before(:each) do
-        @items = [Item.new("Aged Brie", 3, 4), Item.new("Aged Brie", 0, 4), Item.new("Aged Brie", -1, 49), Item.new("Aged Brie", 3, 50)]
+        @items = [
+          Item.new("Aged Brie", 3, 4),
+          Item.new("Aged Brie", 0, 4), 
+          Item.new("Aged Brie", -1, 49), 
+          Item.new("Aged Brie", 3, 50)
+          ]
         @shop = GildedRose.new(@items)
       end
 
@@ -50,37 +59,7 @@ describe GildedRose do
       it "increases quality by 2 when sell_in is below 0" do
         expect {@shop.update_quality }.to change {@items[1].quality}.by 2
       end
-      it "unless increasing by 2 would take quality beyong 50" do
-        expect {@shop.update_quality }.to change {@items[2].quality}.by 1
-      end
-      it "does not increase quality past 50" do
-        expect {@shop.update_quality }.not_to change {@items[3].quality}
-      end
-      
-
-    end
-
-    describe 'for aged brie' do
-
-      before(:each) do
-        @items = [Item.new("Aged Brie", 3, 4), Item.new("Aged Brie", 0, 4), Item.new("Aged Brie", -1, 49), Item.new("Aged Brie", 3, 50)]
-        @shop = GildedRose.new(@items)
-      end
-
-      it "does not change the name" do
-        @shop.update_quality
-        expect(@items[0].name).to eq "Aged Brie"
-      end
-      it "diminishes sellIn by 1" do
-        expect {@shop.update_quality }.to change {@items[0].sell_in }.by -1
-      end
-      it "increases quality by 1 when sell_in is above 0" do
-        expect {@shop.update_quality }.to change {@items[0].quality}.by 1
-      end
-      it "increases quality by 2 when sell_in is below 0" do
-        expect {@shop.update_quality }.to change {@items[1].quality}.by 2
-      end
-      it "unless increasing by 2 would take quality beyong 50" do
+      it "unless increasing by 2 would take quality beyond 50" do
         expect {@shop.update_quality }.to change {@items[2].quality}.by 1
       end
       it "does not increase quality past 50" do
